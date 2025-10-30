f89ae708fbbc08cc6037c824c19efdb67e2ae36c
vinayvadlakondagoud <vinayvadlakondagoudjpp@gmail.com>
2025-10-30 13:04:37 +0530

    Update server.js
    port

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -41,7 +41,7 @@ app.get("/", (req, res) => {
   res.send("Hostel Management Backend Running Successfully!");
 });
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
     
     // ------------------------------------------------------------------
